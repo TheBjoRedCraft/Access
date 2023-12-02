94a97ec1740696e10232e2b48e9138935530b488
Red <red.dev@hexle.at>
2023-12-02 18:57:19 +0100

    added join function

--- src/main/java/de/thebjoredcraft/access/WorldAccessListener.java
+++ src/main/java/de/thebjoredcraft/access/WorldAccessListener.java
@@ -1,9 +1,13 @@
 package de.thebjoredcraft.access;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class WorldAccessListener implements Listener {
@@ -15,6 +19,29 @@ public class WorldAccessListener implements Listener {
             AccessManager.denied(target);
         }else{
             AccessManager.allowed(target);
+        }
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Player target = event.getPlayer();
+        int worldCount = Bukkit.getWorlds().size();
+        int currentCount = 0;
+
+        if(target.hasPermission("access.worlds." + target.getWorld().getName())){
+            AccessManager.allowed(target);
+        }else{
+            for(World targetWorld : Bukkit.getWorlds()){
+                currentCount ++;
+                if(target.hasPermission("access.worlds." + targetWorld.getName())){
+                    target.teleport(targetWorld.getSpawnLocation());
+                    AccessManager.allowed(target);
+                    return;
+                }
+                if(currentCount == worldCount){
+                    target.kick(MiniMessage.miniMessage().deserialize("<bold>Es wurde keine Welt gefunden, zu der du Zugriff hast!"));
+                    currentCount = 0;
+                }
+            }
         }
     }
 }
